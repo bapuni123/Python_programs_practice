dbf0a6169c996fefe446b79baff00cfdb5e47800
bapuni123 <asheskb123@gmail.com>
2020-07-21 06:37:45 +0530

    modified fact_recur.py

--- fact_recur.py
+++ fact_recur.py
@@ -3,6 +3,5 @@ def factorial(n):
     return 1 if (n==1 or n==0) else n * factorial(n - 1);  
     
 
-num = 5; 
-print("Factorial of",num,"is", 
-factorial(num)) 
+num = int(input("Enter a number:"))
+print("Factorial of",num,"is", factorial(num))
